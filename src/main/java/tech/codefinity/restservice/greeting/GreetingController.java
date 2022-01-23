df69993b8b3e9f1fd7693e5cc3e43016c4515b1c
Manav Misra <manavm@visionwebsoft.com>
2022-01-23 12:20:56 -0600

    :bulb:

--- src/main/java/tech/codefinity/restservice/greeting/GreetingController.java
+++ src/main/java/tech/codefinity/restservice/greeting/GreetingController.java
@@ -1,19 +1,21 @@
 package tech.codefinity.restservice.greeting;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+// @RestController - marks the class as a controller where every method returns a domain object
+// instead of a dumb, stupid view
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+  private static final String template = "Hello, %s!";
+  private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }
+  @GetMapping("/greeting")
+  public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return new Greeting(counter.incrementAndGet(), String.format(template, name));
+  }
 }
